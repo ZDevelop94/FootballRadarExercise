38f9df5b83e87b41955b1a36d7359a8b08d6f648
Zephaniah Flynn <18018780+ZDevelop94@users.noreply.github.com>
2025-07-18 16:17:40 +0100

    optimised algorithm and reduced boilerplate

--- src/main/scala/service/MatchDaysCalcService.scala
+++ src/main/scala/service/MatchDaysCalcService.scala
@@ -5,18 +5,36 @@ import domain.{LeagueTable, Match}
 
 class MatchDaysCalcService {
 
+  private lazy val homeGamesWon: Match => Boolean = `match` => `match`.homeGoals > `match`.awayGoals
+  private lazy val awayGamesWon: Match => Boolean = `match` => `match`.homeGoals > `match`.awayGoals
+
+  lazy val homeGamesLost: Match => Boolean = `match` => `match`.homeGoals < `match`.awayGoals
+  lazy val awayGamesLost: Match => Boolean = `match` => `match`.awayGoals < `match`.homeGoals
+
+  lazy val gamesDrawn: Match => Boolean = `match` => `match`.homeGoals == `match`.awayGoals
+
   def calculateTable(matches: Vector[Match]): IO[LeagueTable] = {
     val distinctHomeTeams = matches.groupBy(_.homeTeam)
     val distinctAwayTeams = matches.groupBy(_.awayTeam)
 
-    val allTeamsMatchesWon = distinctHomeTeams.map(calculateHomeGamesWon) ++ distinctAwayTeams.map(calculateAwayGamesWon)
-    val allTeamsMatchesLost = distinctHomeTeams.map(calculateHomeGamesLost) ++ distinctAwayTeams.map(calculateAwayGamesLost)
-    val allTeamsMatchesDrawn = distinctHomeTeams.map(calculateGamesDrawn) ++ distinctAwayTeams.map(calculateGamesDrawn)
+    val allTeamsMatchesWon = distinctHomeTeams.map { (teamName, matches) =>
+      calculateGame(teamName, matches)(homeGamesWon)
+    } ++ distinctAwayTeams.map { (teamName, matches) =>
+      calculateGame(teamName, matches)(awayGamesWon)
+    }
+
+    val allTeamsMatchesLost = distinctHomeTeams.map { (teamName, matches) =>
+      calculateGame(teamName, matches)(homeGamesLost)
+    } ++ distinctAwayTeams.map(calculateAwayGamesLost)
+
+    val allTeamsMatchesDrawn = (distinctHomeTeams ++ distinctAwayTeams).map { (teamName, matches) =>
+      calculateGame(teamName, matches)(gamesDrawn)
+    }
 
     ???
   }
 
-  private def calculateHomeGamesWon(teamName: String, matches: Vector[Match]): (String, Int) = {
+  private def calculateGame(teamName: String, matches: Vector[Match])(f: Match => Boolean ): (String, Int) = {
     (teamName,
       matches.count { `match` =>
         `match`.homeGoals > `match`.awayGoals
